ad7126212ce622b5efb5b33a8fd1983fb9af600a
Aphirat Nimanussonkul <Tata@odds.team>
2020-10-11 09:59:44 +0700

    move setting data onViewCreated, it's the right way"

--- app/src/main/java/bluebuildx/voc/HomeFragment.kt
+++ app/src/main/java/bluebuildx/voc/HomeFragment.kt
@@ -9,49 +9,39 @@ import androidx.viewpager.widget.ViewPager
 import bluebuildx.voc.adapter.LevelOptionAdapter
 import bluebuildx.voc.models.LevelOptionModel
 
-
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var homeView: View = inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         var imageUrl: String =
             "https://image.freepik.com/free-vector/flat-children-s-day-kids-with-games-candies_23-2148324112.jpg"
-        var viewPager: ViewPager = homeView.findViewById(R.id.view_pager)
+        var viewPager: ViewPager = view.findViewById(R.id.view_pager)
         var adapter: LevelOptionAdapter
 
-        var models = ArrayList<LevelOptionModel>();
-        models.add(
+        val models = arrayListOf(
+            LevelOptionModel(
+                imageUrl, "ง่าย", "ระดับคำศัพย์ไม่ยากมากนัก\n" +
+                        "เป็นคำศัพย์พื้นฐานที่ใช้ทั่วๆ ไป"
+            ),
+            LevelOptionModel(
+                imageUrl, "ปานกลาง", "ระดับคำศัพย์ไม่ยากมากนัก\n" +
+                        "เป็นคำศัพย์พื้นฐานที่ใช้ทั่วๆ ไป"
+            ),
             LevelOptionModel(
                 imageUrl, "ยาก", "ระดับคำศัพย์ไม่ยากมากนัก\n" +
                         "เป็นคำศัพย์พื้นฐานที่ใช้ทั่วๆ ไป"
             )
         )
-        models.add(
-            LevelOptionModel(
-                imageUrl, "ปานกลาง", "ระดับคำศัพย์ไม่ยากมากนัก\n" +
-                        "เป็นคำศัพย์พื้นฐานที่ใช้ทั่วๆ ไป"
-            )
-        )
-        models.add(
-            LevelOptionModel(
-                imageUrl, "ง่าย", "ระดับคำศัพย์ไม่ยากมากนัก\n" +
-                        "เป็นคำศัพย์พื้นฐานที่ใช้ทั่วๆ ไป"
-            )
-        )
+
         adapter = LevelOptionAdapter(models, this.context);
-
-        viewPager.setPadding(80,32,80,32);
+        viewPager.setPadding(80, 32, 80, 32);
         viewPager.adapter = adapter
-
-        return homeView
     }
 }
 
